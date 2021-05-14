8ecceb334b6f34597695df5f91daf819c6793c72
tanabetetsuya <tetsumagic123@gmail.com>
2021-05-14 12:31:01 +0000

    [Start] 4-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e27d4cc5cda149504493d999e6a226dfc182f857e4d145b5dc0354e591c2829a7b9965cbbeaff94abcde51bdd25b103f18a7c6881fee2770a240f43f02289965'
